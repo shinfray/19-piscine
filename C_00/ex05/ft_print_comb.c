3c21582704a80c6c17f6fc2250c94404af51d611
Simon Hinfray <shinfray@student.s19.be>
2022-07-07 20:45:20 +0200

    ex05

---
+++ C_00/ex05/ft_print_comb.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shinfray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 18:35:39 by shinfray          #+#    #+#             */
+/*   Updated: 2022/07/07 20:08:27 by shinfray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+	if (a != '7')
+		write (1, ", ", 2);
+}
+
+void	ft_print_comb(void)
+{
+	char	a;
+	char	b;
+	char	c;
+
+	a = '0';
+	b = '1';
+	c = '2';
+
+	while ( a <= '7')
+	{
+		while ( b <= '8')
+		{
+			while ( c <= '9')
+			{
+				print(a, b, c);
+				c++;
+			}
+			b++;
+			c = b + 1;
+		}
+		a++;
+		b = a + 1;
+		c = b + 1;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb();
+}
