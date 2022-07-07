b1071a82cd460133b2e41c6b3f8aa8fd9e59218c
Simon Hinfray <shinfray@student.s19.be>
2022-07-07 23:01:27 +0200

    add ex 06

---
+++ C_00/ex06/ft_print_comb2.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shinfray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 22:38:44 by shinfray          #+#    #+#             */
+/*   Updated: 2022/07/07 23:00:11 by shinfray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 2);
+}
+
+int	print(char a, char b, char c, char b))
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(' ');
+	ft_putchar(c);
+	ft_putchar(d);
+}
