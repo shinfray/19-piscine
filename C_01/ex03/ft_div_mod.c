5ba11510b9d4f367b906e01917709f89c7a9fcc3
Simon Hinfray <shinfray@student.s19.be>
2022-07-10 19:39:23 +0200

    update ex03

---
+++ C_01/ex03/ft_div_mod.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fit_div_mod.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shinfray <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/10 19:02:50 by shinfray          #+#    #+#             */
+/*   Updated: 2022/07/10 19:32:30 by shinfray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//	#include <stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+
+//	int	main(void)
+//	{
+//		int	a;
+//		int	b;
+//		int	div;
+//		int	mod;
+//	
+//		a = 74;
+//		b = 5;
+//		div = 0;
+//		mod = 0;
+//		ft_div_mod(a, b, &div, &mod);
+//		printf("a = %d et b = %d.\nLa division donne %d.\n", a, b, div);
+//		printf("le reste est %d.\n", mod);
+//	}
