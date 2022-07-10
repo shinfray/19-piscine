41d06c281050ffcb77083358d816560e8dee3b0f
Simon Hinfray <shinfray@student.s19.be>
2022-07-10 20:00:47 +0200

    add ex 04

--- C_01/ex04/ft_ultimate_div_mod.c
+++ C_01/ex04/ft_ultimate_div_mod.c
@@ -6,8 +6,29 @@
 /*   By: shinfray <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 19:36:53 by shinfray          #+#    #+#             */
-/*   Updated: 2022/07/10 19:37:05 by shinfray         ###   ########.fr       */
+/*   Updated: 2022/07/10 19:59:51 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//	#include <stdio.h>
 
+void	ft_ultimate_div_mod(int *a, int*b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *a / *b;
+	*b = temp % *b;
+}
+
+//	int	main(void)
+//	{
+//		int	a;
+//		int	b;
+//	
+//		a = 56;
+//		b = 3;
+//		printf("a = %d et b = %d.\n", a, b);
+//		ft_ultimate_div_mod(&a, &b);
+//		printf("div = %d, et mod = %d.\n", a, b);
+//	}
