05c1cd1982f85d2214ae024f74824aa96ff82ffb
Simon Hinfray <shinfray@student.s19.be>
2022-07-13 09:38:44 +0200

    update ex04

--- C_03/ex04/ft_strstr.c
+++ C_03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: shinfray <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 22:27:47 by shinfray          #+#    #+#             */
-/*   Updated: 2022/07/12 23:52:55 by shinfray         ###   ########.fr       */
+/*   Updated: 2022/07/13 09:37:30 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ char	*ft_strstr(char *str, char *to_find)
 
 	i = 0;
 	j = 0;
+	if (to_find[i] == '\0')
+		return (str);
 	while (str[i])
 	{
 		while (to_find[j] == str[i + j])
@@ -28,6 +30,7 @@ char	*ft_strstr(char *str, char *to_find)
 			if (to_find[j] == '\0')
 				return (str + i);
 		}
+		j = 0;
 		i++;
 	}
 	return (0);
@@ -36,8 +39,8 @@ char	*ft_strstr(char *str, char *to_find)
 /*
 int	main(void)
 {
-	char	str[] = "comment ca va aujOurd'hui ?";
-	char	to_find[] = "aujour";
+	char	str[] = "coucoul";
+	char	to_find[] = "coul";
 
 	printf("%s\n", ft_strstr(str, to_find));
 //	printf("%s\n", strstr(str, to_find));
